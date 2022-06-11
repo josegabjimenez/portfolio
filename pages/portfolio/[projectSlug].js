b7bccd158e998d13a045525d310e2823986d830d
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-06-11 12:18:52 -0500

    :construction: Working on Project Detail View

--- pages/portfolio/[projectSlug].js
+++ pages/portfolio/[projectSlug].js
@@ -3,10 +3,53 @@ import endPoints from '@services/endPoints';
 
 const Project = ({ project }) => {
   return (
-    <div>
-      <h1>Project</h1>
-      <p>{project.title}</p>
-    </div>
+    <main className="flex flex-col justify-center items-center">
+      <section className="hero h-80 shadow-[inset_0_0_15px_2px_rgba(0,0,0,0.3)]" style={{ backgroundImage: `url(${project.images[0]})` }}>
+        <div className="hero-overlay bg-opacity-60" />
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">{project.title}</h1>
+          </div>
+        </div>
+      </section>
+      <section className="mt-16 w-1/3 text-justify">
+        <h1>Project</h1>
+        <h1>{project.description}</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, assumenda laboriosam ea, beatae excepturi totam hic harum soluta sapiente sit, dicta inventore nesciunt! Animi asperiores
+          aperiam saepe, a modi officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, assumenda laboriosam ea, beatae excepturi totam hic harum soluta sapiente sit, dicta inventore nesciunt! Animi asperiores
+          aperiam saepe, a modi officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, assumenda laboriosam ea, beatae excepturi totam hic harum soluta sapiente sit, dicta inventore nesciunt! Animi asperiores
+          aperiam saepe, a modi officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, assumenda laboriosam ea, beatae excepturi totam hic harum soluta sapiente sit, dicta inventore nesciunt! Animi asperiores
+          aperiam saepe, a modi officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, assumenda laboriosam ea, beatae excepturi totam hic harum soluta sapiente sit, dicta inventore nesciunt! Animi asperiores
+          aperiam saepe, a modi officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, assumenda laboriosam ea, beatae excepturi totam hic harum soluta sapiente sit, dicta inventore nesciunt! Animi asperiores
+          aperiam saepe, a modi officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, assumenda laboriosam ea, beatae excepturi totam hic harum soluta sapiente sit, dicta inventore nesciunt! Animi asperiores
+          aperiam saepe, a modi officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, assumenda laboriosam ea, beatae excepturi totam hic harum soluta sapiente sit, dicta inventore nesciunt! Animi asperiores
+          aperiam saepe, a modi officia.
+        </p>
+      </section>
+      {/* <p>{project.title}</p> */}
+    </main>
   );
 };
 
