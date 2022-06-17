b9f2b7b5cf61cfb8aaf42efb2d6c837e4f711412
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-06-17 14:08:41 -0500

    :heavy_minus_sign: A deprecated dependency was removed (react-firebaseui)

--- pages/portfolio/[projectSlug].js
+++ pages/portfolio/[projectSlug].js
@@ -6,6 +6,9 @@ import { Hero } from '@components/index';
 //Icons
 import { MdDone } from 'react-icons/md';
 import { RiToolsFill, RiGithubFill, RiEyeFill } from 'react-icons/ri';
+//Carousel CSS
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
 
 const Project = ({ project }) => {
   const size = useWindowSize();
@@ -64,51 +67,8 @@ const Project = ({ project }) => {
 
           {/* Images carousel */}
           <h3 className="text-2xl font-bold mt-8">Some images</h3>
-          <div className="carousel w-full h-80">
-            <div id="slide1" className="carousel-item relative w-full">
-              <img src="https://api.lorem.space/image/car?w=800&h=200&hash=8B7BCDC2" className="w-full" />
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide4" className="btn btn-circle">
-                  ⬅️
-                </a>
-                <a href="#slide2" className="btn btn-circle">
-                  ➡️
-                </a>
-              </div>
-            </div>
-            <div id="slide2" className="carousel-item relative w-full">
-              <img src="https://api.lorem.space/image/car?w=800&h=200&hash=500B67FB" className="w-full" />
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide1" className="btn btn-circle">
-                  ⬅️
-                </a>
-                <a href="#slide3" className="btn btn-circle">
-                  ➡️
-                </a>
-              </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full">
-              <img src="https://api.lorem.space/image/car?w=800&h=200&hash=A89D0DE6" className="w-full" />
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide2" className="btn btn-circle">
-                  ⬅️
-                </a>
-                <a href="#slide4" className="btn btn-circle">
-                  ➡️
-                </a>
-              </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-              <img src="https://api.lorem.space/image/car?w=800&h=200&hash=225E6693" className="w-full" />
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide3" className="btn btn-circle">
-                  ⬅️
-                </a>
-                <a href="#slide1" className="btn btn-circle">
-                  ➡️
-                </a>
-              </div>
-            </div>
+
+   
           </div>
         </div>
       </section>
