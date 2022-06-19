0f728d28654a3d4900b3cff5ed2aa72b43895b0a
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-06-19 16:45:14 -0500

    :children_crossing: Navbar and Footer updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,17 +2,34 @@ import React from 'react';
 import { AiFillGithub, AiFillLinkedin, AiFillMail } from 'react-icons/ai';
 
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const links = {
+    github: 'https://github.com/josegabjimenez',
+    linkedin: 'https://www.linkedin.com/in/jose-gabriel-jim%C3%A9nez-vidales-1b0179215/',
+    email: 'mailto:josegabojimenez@gmail.com',
+  };
   return (
-    <footer>
+    <footer className="">
       <section className="flex justify-center text-4xl gap-6">
-        <AiFillGithub />
-        <AiFillLinkedin />
-        <AiFillMail />
+        <a className="transition-colors hover:text-primary" href={links.github} target="_blank" rel="noreferrer">
+          <AiFillGithub />
+        </a>
+        <a className="transition-colors hover:text-primary" href={links.linkedin} target="_blank" rel="noreferrer">
+          <AiFillLinkedin />
+        </a>
+
+        <a className="transition-colors hover:text-primary" href={links.email} target="_blank" rel="noreferrer">
+          <AiFillMail />
+        </a>
       </section>
       <section className="footer footer-center p-4 mt-4 text-base-content">
         <div>
           <p>
-            Copyright © 2022 - Made with 💓 by <i className="font-bold underline">@josegabjimenez</i>
+            Copyright © {year} - Made with 💓 by{' '}
+            <a href={links.github} target="_blank" rel="noreferrer" className="transition-colors hover:text-primary font-bold underline">
+              @josegabjimenez
+            </a>
           </p>
         </div>
       </section>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import { GrCode } from 'react-icons/gr';
 import { CgCodeSlash } from 'react-icons/cg';
+import { RiDownloadCloudLine } from 'react-icons/ri';
 
 const Navbar = () => {
   const routes = [
@@ -64,10 +65,11 @@ const Navbar = () => {
       <div className="navbar-end mr-4">
         {/* Contact button */}
         <a className="btn btn-outline btn-secondary rounded-full">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+          {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-          </svg>
-          Contact me
+          </svg> */}
+          <RiDownloadCloudLine className="h-5 w-5 mr-1" />
+          Download CV
         </a>
       </div>
     </div>
