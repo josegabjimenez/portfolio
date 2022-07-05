63d53d20389bb4e83eb52efd78de5d951ec6ed06
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-07-04 22:13:13 -0500

    :construction: Working on about section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+// import Image from 'next/image';
 
 const Hero = ({ data }) => {
   return (
     <section className="hero overflow-hidden absolute top-0 -z-[1] h-1/2">
       {/* style={{ backgroundImage: `url(${project.images[0]})` }} */}
+      {/* <div className="w-full absolute -z-[1] blur-sm">
+        <Image width="100%" height="100%" layout="fill" src={data.images[0]} alt="Project Background Image" />
+      </div> */}
       <img className="w-full absolute -z-[1] blur-sm" src={data.images[0]} alt="Project Background Image" />
       <div className="hero-overlay bg-opacity-80 " />
       <div className="hero-content text-center">
