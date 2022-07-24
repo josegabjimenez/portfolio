e5f470051c16c0cf39f629ce95fd92853b70c608
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2022-07-24 15:49:40 -0500

    :mag: SEO Improved

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content="Jose Gabriel's Portfolio" />
+          <link rel="icon" href="/coding.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
