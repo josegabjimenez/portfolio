327f98ac81f570c5f5b2f9068654874e07fe5dad
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2023-07-26 19:57:22 -0500

    :chart_with_upwards_trend: feat: add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '@styles/globals.css';
 import { Layout } from '@components/index';
 
+// Analytics
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </>
   );
